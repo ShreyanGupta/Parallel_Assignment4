8a6d45119b64cdb977190d43bde7339bda1123dd
Shreyan <shreyan.gupta96@gmail.com>
2017-05-07 02:41:17 +0530

    parsing done, ultra basic kernel working

--- a4.cu
+++ a4.cu
@@ -1,44 +1,107 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <string>
+#include <cstdlib>
+// #include <string>
 #include <cuda.h>
 using namespace std;
 
+// module load compiler/cuda/7.5/compilervars
+// module load compiler/gcc/4.9.3/compilervars
+// module load mpi/mpich/3.1.4/gcc/mpivars
+// module load apps/lammps/gpu
+
 int dim = -1;
-vector<int> ptr, index, data;
-vector<int> B;
+vector<int> ptr, indices, data;
+vector<int> B,C;
+
+int *d_dim;
+int *d_ptr, *d_indices, *d_data;
+int *d_B, *d_C;
 
 void parse_input(string file){
-	ifstream fin(file);
+
+	// string input in ifstream not working. Don't ask why
+	// stoi not working. Also don't ask why
+	ifstream fin(file.c_str());
 	string r,c,d,temp;
 	fin >> temp >> temp >> temp >> dim >> temp;
 	fin >> r >> c >> d;
-	B = vector<int>(d);
-	int current_row = 0
+	B = vector<int>(dim);
+	C = vector<int>(dim);
+	int current_row = 0;
 	int local_ptr = 0;
 	ptr.push_back(0);
-	// ptr = vector<int>(current_row+1, local_ptr);
 
 	while(r[0] != 'B'){
-		int this_row = stoi(r);
+		// cout << r << " " << c << " " << d << endl;
+		int this_row = atoi(r.c_str());
 		for(int i=current_row; i<this_row; ++i) ptr.push_back(local_ptr);
-		index.push_back(stoi(c));
-		data.push_back(stoi(d));
+		indices.push_back(atoi(c.c_str()));
+		data.push_back(atoi(d.c_str()));
+		current_row = this_row;
 		++local_ptr;
 		fin >> r >> c >> d;
 	}
 	ptr.push_back(local_ptr);
-	B[0] = stoi(c); B[1] = stoi(d);
+	B[0] = atoi(c.c_str());
+	B[1] = atoi(d.c_str());
 	for(int i=2; i<dim; ++i) fin >> B[i];
 
-	for(auto i : ptr) cout << i << " "; cout << endl;
-	for(auto i : index) cout << i << " "; cout << endl;
-	for(auto i : data) cout << i << " "; cout << endl;
+	// auto doesn't work for some reason...
+	for(int i=0; i<ptr.size(); ++i) 	cout << ptr[i] << " "; cout << endl;
+	for(int i=0; i<indices.size(); ++i) cout << indices[i] << " "; cout << endl;
+	for(int i=0; i<data.size(); ++i) 	cout << data[i] << " "; cout << endl;
+	for(int i=0; i<B.size(); ++i) 		cout << B[i] << " "; cout << endl;
+}
+
+void init(){
+	cudaMalloc((void **)&d_dim, sizeof(int));
+	cudaMalloc((void **)&d_ptr, ptr.size() * sizeof(int));
+	cudaMalloc((void **)&d_indices, indices.size() * sizeof(int));
+	cudaMalloc((void **)&d_data, data.size() * sizeof(int));
+	cudaMalloc((void **)&d_B, B.size() * sizeof(int));
+	cudaMalloc((void **)&d_C, C.size() * sizeof(int));
+
+	cudaMemcpy(d_dim, &dim, sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_ptr, &ptr[0], ptr.size() * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_indices, &indices[0], indices.size() * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_data, &data[0], data.size() * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_B, &B[0], B.size() * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_C, &C[0], C.size() * sizeof(int), cudaMemcpyHostToDevice);
+}
+
+void anti_init(){
+	cudaMemcpy(&C[0], d_C, C.size() * sizeof(int), cudaMemcpyDeviceToHost);
+
+	cudaFree(d_dim);
+	cudaFree(d_ptr);
+	cudaFree(d_indices);
+	cudaFree(d_data);
+	cudaFree(d_B);
+}
+
+__global__ void kernel(
+		int *dim, 
+		int *ptr, 
+		int *indices, 
+		int *data,
+		int *B,
+		int *C)
+{
+	int tid = threadIdx.x;
+	C[tid] = B[tid];
 }
 
 int main(int argc, char const *argv[])
 {
-	
+	string file = "input1.txt";
+	parse_input(file);
+	init();
+
+	kernel<<<1,dim>>>(d_dim, d_ptr, d_indices, d_data, d_B, d_C);
+
+	anti_init();
+	for(int i=0; i<C.size(); ++i) cout << C[i] << " "; cout << endl;
 	return 0;
 }
